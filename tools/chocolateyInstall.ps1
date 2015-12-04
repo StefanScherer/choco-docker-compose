0d7b0a8c39501e2be5ae5764e0bdc85dab2bfc92
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-04 07:39:15 +0100

    Fix install script, code got lost

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -6,4 +6,9 @@ $checksum64 = $checksum
 $checksumType = 'md5'
 $checksumType64 = $checksumType
 
-ker
+$destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$file = "$($destination)\docker-compose.exe"
+
+if (![System.IO.Directory]::Exists($destination)) {[System.IO.Directory]::CreateDirectory($destination)}
+
+Get-ChocolateyWebFile $packageName $file $url $url64 $checksum $checksum64 $checksumType $checksumType64
