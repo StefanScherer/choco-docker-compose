a68911a78405305033becaf4f0835607435d890f
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-04 07:36:27 +0100

    Update docker-compose 1.5.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,14 +1,9 @@
 $packageName = 'docker-compose'
-$url = 'https://github.com/docker/compose/releases/download/1.5.1/docker-compose-Windows-x86_64.exe'
+$url = 'https://github.com/docker/compose/releases/download/1.5.2/docker-compose-Windows-x86_64.exe'
 $url64 = $url
-$checksum = 'e23e5c5a4f456aa901ef06e85ead950d'
+$checksum = '4640306ae527aa5876ddc0f31b7e1c5b'
 $checksum64 = $checksum
 $checksumType = 'md5'
 $checksumType64 = $checksumType
 
-$destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file = "$($destination)\docker-compose.exe"
-
-if (![System.IO.Directory]::Exists($destination)) {[System.IO.Directory]::CreateDirectory($destination)}
-
-Get-ChocolateyWebFile $packageName $file $url $url64 $checksum $checksum64 $checksumType $checksumType64
+ker
