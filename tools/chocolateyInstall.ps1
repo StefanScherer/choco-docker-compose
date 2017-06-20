5c0f84c13210b81f84142a84feb2a97a056ce37f
Stefan Scherer <scherer_stefan@icloud.com>
2017-06-20 07:30:16 +0200

    Update docker-compose 1.14.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'docker-compose'
-$url = 'https://github.com/docker/compose/releases/download/1.13.0/docker-compose-Windows-x86_64.exe'
+$url = 'https://github.com/docker/compose/releases/download/1.14.0/docker-compose-Windows-x86_64.exe'
 $url64 = $url
-$checksum = 'b933e653cd77f1d990d4dda598f1cc8fe8f128d66485569e828c0976602a8d5e'
+$checksum = 'bfb8058aac1bf6f8b0e44b70e69dc89d89e4493a3ba43d3ec46b3aad54512da8'
 $checksum64 = $checksum
 $checksumType = 'sha256'
 $checksumType64 = $checksumType
