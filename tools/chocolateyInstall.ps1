a1324214ebc0f3a4069925e4da9c8239cff26df3
Rui Lopes <rgl@ruilopes.com>
2021-02-27 16:18:54 +0000

    Update docker-compose 1.28.5

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'docker-compose'
-$url = 'https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Windows-x86_64.exe'
-$checksum = 'ad808a91fbf782bc8a306017df450c36e77edcbea139ef0d036642568622a326'
+$url = 'https://github.com/docker/compose/releases/download/1.28.5/docker-compose-Windows-x86_64.exe'
+$checksum = '458ff856310a38ad3365d644518f617e926d06f52f12f2b7c0bcff08c56d461e'
 $checksumType = 'sha256'
 
 $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
